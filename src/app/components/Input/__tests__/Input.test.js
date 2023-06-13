7675227eefe2620302e34149468894474bee9638
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2023-06-13 13:49:54 -0400

    EDD-17: fix react warning on act()

--- src/app/components/Input/__tests__/Input.test.js
+++ src/app/components/Input/__tests__/Input.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-// import userEvent from '@testing-library/user-event'
 
 import Input from '../Input'
 
--- src/app/dialogs/Settings/__tests__/Settings.test.js
+++ src/app/dialogs/Settings/__tests__/Settings.test.js
@@ -1,33 +1,35 @@
 import React from 'react'
 
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import { act } from 'react-dom/test-utils'
 
 import Settings from '../Settings'
 import { ElectronApiContext } from '../../../context/ElectronApiContext'
 
-const setup = () => {
-  // we need to mock hasActiveDownloads
-  const mockHasActiveDownloads = true
+const setup = (hasActiveDownloads) => {
+  // we need to mock hasActiveDownloads to render warning
+  const mockHasActiveDownloads = hasActiveDownloads
   const chooseDownloadLocation = jest.fn()
   const setDownloadLocation = jest.fn()
   const setPreferenceFieldValue = jest.fn()
   const getPreferenceFieldValue = () => 5
-
-  render(
-    <ElectronApiContext.Provider value={{
-      chooseDownloadLocation,
-      setDownloadLocation,
-      setPreferenceFieldValue,
-      getPreferenceFieldValue
-    }}
-    >
-      <Settings
-        hasActiveDownloads={mockHasActiveDownloads}
-      />
-    </ElectronApiContext.Provider>
-  )
+  act(() => {
+    render(
+      <ElectronApiContext.Provider value={{
+        chooseDownloadLocation,
+        setDownloadLocation,
+        setPreferenceFieldValue,
+        getPreferenceFieldValue
+      }}
+      >
+        <Settings
+          hasActiveDownloads={mockHasActiveDownloads}
+        />
+      </ElectronApiContext.Provider>
+    )
+  })
 
   return {
     chooseDownloadLocation,
@@ -38,65 +40,96 @@ const setup = () => {
 }
 
 describe('Settings dialog', () => {
-  test('renders the Settings modal page including the element if there are active downloads', () => {
-    setup()
-    expect(screen.getByTestId('settings-hasActiveDownloads')).toBeInTheDocument()
+  test('renders the Settings modal page including the element if there are active downloads', async () => {
+    const hasActiveDownloads = true
+    setup(hasActiveDownloads)
+    await waitFor(() => {
+      expect(screen.getByTestId('settings-hasActiveDownloads')).toBeInTheDocument()
+    })
   })
 
-  test('clicking the download location sends a message to the main process', async () => {
-    const user = userEvent.setup()
+  test('renders the Settings modal page excluding active downloads', async () => {
+    const hasActiveDownloads = false
+    setup(hasActiveDownloads)
+    await waitFor(() => {
+    // Can't use getByTestId, that returns an error instead of null
+      expect(screen.queryByTestId('settings-hasActiveDownloads')).not.toBeInTheDocument()
+    })
+  })
 
-    const { chooseDownloadLocation } = setup()
+  test('Clicking the download location sends a message to the main process', async () => {
+    const user = userEvent.setup()
+    const hasActiveDownloads = true
+
+    const { chooseDownloadLocation } = setup(hasActiveDownloads)
 
     await user.click(screen.getByTestId('initialize-download-location'))
 
-    expect(chooseDownloadLocation).toHaveBeenCalledTimes(1)
+    await waitFor(() => {
+      expect(screen.getByTestId('initialize-download-location')).toBeInTheDocument()
+      expect(chooseDownloadLocation).toHaveBeenCalledTimes(1)
+    })
   })
 
-  test('clicking on the `Clear default download location` sends a message to the main process', async () => {
+  test('Clicking on the `Clear default download location` sends a message to the main process', async () => {
     const user = userEvent.setup()
-    const { setPreferenceFieldValue } = setup()
+    const hasActiveDownloads = true
+    const { setPreferenceFieldValue } = setup(hasActiveDownloads)
 
     await user.click(screen.getByTestId('settings-clear-default-download'))
 
-    expect(setPreferenceFieldValue).toHaveBeenCalledTimes(1)
-    expect(setPreferenceFieldValue).toHaveBeenCalledWith('defaultDownloadLocation', undefined)
+    await waitFor(() => {
+      expect(setPreferenceFieldValue).toHaveBeenCalledTimes(1)
+      expect(setPreferenceFieldValue).toHaveBeenCalledWith('defaultDownloadLocation', undefined)
+    })
   })
 
   test('Typing into the input field for the concurrency will trigger a call to main process', async () => {
     const user = userEvent.setup()
+    const hasActiveDownloads = true
 
-    const { setPreferenceFieldValue } = setup()
+    const { setPreferenceFieldValue } = setup(hasActiveDownloads)
 
     await user.type(screen.getByTestId('input-test-id'), '{backspace}')
     await user.type(screen.getByTestId('input-test-id'), '3')
     // trigger blur by leaving the text field
     await user.tab()
-    expect(setPreferenceFieldValue).toHaveBeenCalledTimes(1)
-    // Ensure that the string argument to being converted to a numerical
-    expect(setPreferenceFieldValue).toHaveBeenCalledWith('concurrentDownloads', 3)
+
+    await waitFor(() => {
+      expect(setPreferenceFieldValue).toHaveBeenCalledTimes(1)
+      // Ensure that the string argument to being converted to a numerical
+      expect(setPreferenceFieldValue).toHaveBeenCalledWith('concurrentDownloads', 3)
+    })
   })
 
   test('Typing a value < 1 or characters into the text field will result in no calls', async () => {
     const user = userEvent.setup()
-    const { setPreferenceFieldValue } = setup()
+    const hasActiveDownloads = true
+    const { setPreferenceFieldValue } = setup(hasActiveDownloads)
     await user.type(screen.getByTestId('input-test-id'), '{backspace}')
     await user.type(screen.getByTestId('input-test-id'), '-')
     await user.type(screen.getByTestId('input-test-id'), 'a')
     await user.type(screen.getByTestId('input-test-id'), '0')
     // trigger blur by leaving the text field
     await user.tab()
-    // negative values or non-numerical changes to the input should not attempt to change the json store
-    expect(setPreferenceFieldValue).toHaveBeenCalledTimes(0)
+
+    await waitFor(() => {
+      // negative values or non-numerical changes to the input should not attempt to change the json store
+      expect(setPreferenceFieldValue).toHaveBeenCalledTimes(0)
+    })
   })
 
   test('Deleting the value in the field and leaving focus will not call a store change', async () => {
     const user = userEvent.setup()
-    const { setPreferenceFieldValue } = setup()
+    const hasActiveDownloads = true
+    const { setPreferenceFieldValue } = setup(hasActiveDownloads)
     await user.type(screen.getByTestId('input-test-id'), '{backspace}')
     // trigger blur by leaving the text field
     await user.tab()
+
+    await waitFor(() => {
     // leaving the text field empty will not update the store
-    expect(setPreferenceFieldValue).toHaveBeenCalledTimes(0)
+      expect(setPreferenceFieldValue).toHaveBeenCalledTimes(0)
+    })
   })
 })

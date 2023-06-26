faa08f9c8c1577e4b37c6651255c03ec7c7adbd8
Matthew Crouch <matthew@element84.com>
2023-06-26 14:51:47 -0400

    EDD-6: Fixes linter warnings

--- migrations/20230613175153_create_preferences.js
+++ migrations/20230613175153_create_preferences.js
@@ -1,15 +1,11 @@
-exports.up = function (knex) {
-  return knex.schema
-    .createTable('preferences', (table) => {
-      table.increments('id')
-      table.string('lastDownloadLocation', 255)
-      table.string('defaultDownloadLocation', 255)
-      table.integer('concurrentDownloads')
-      table.jsonb('windowState')
-    })
-}
+exports.up = (knex) => knex.schema
+  .createTable('preferences', (table) => {
+    table.increments('id')
+    table.string('lastDownloadLocation', 255)
+    table.string('defaultDownloadLocation', 255)
+    table.integer('concurrentDownloads')
+    table.jsonb('windowState')
+  })
 
-exports.down = function (knex) {
-  return knex.schema
-    .dropTable('preferences')
-}
+exports.down = (knex) => knex.schema
+  .dropTable('preferences')

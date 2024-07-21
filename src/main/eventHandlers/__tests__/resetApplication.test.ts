7088e5f7065d7e0cd6bdb10776fb7ea36b1754fa
Matthew Crouch <matthew@element84.com>
2024-07-20 20:55:00 -0400

    EDD-64: Adds tests

--- src/main/eventHandlers/__tests__/resetApplication.test.ts
+++ src/main/eventHandlers/__tests__/resetApplication.test.ts
@@ -13,26 +13,56 @@ jest.mock('electronShell', () => ({
 }))
 
 describe('resetApplication', () => {
-  test('deletes the database and relaunches the application', async () => {
-    const database = {
-      destroy: jest.fn()
-    }
+  describe('when the reset succeeds', () => {
+    test('deletes the database and relaunches the application', async () => {
+      const database = {
+        destroy: jest.fn()
+      }
 
-    await resetApplication({ database })
+      await resetApplication({ database })
 
-    expect(database.destroy).toHaveBeenCalledTimes(1)
-    expect(database.destroy).toHaveBeenCalledWith()
+      expect(database.destroy).toHaveBeenCalledTimes(1)
+      expect(database.destroy).toHaveBeenCalledWith()
 
-    expect(app.getPath).toHaveBeenCalledTimes(1)
-    expect(app.getPath).toHaveBeenCalledWith('userData')
+      expect(app.getPath).toHaveBeenCalledTimes(1)
+      expect(app.getPath).toHaveBeenCalledWith('userData')
 
-    expect(shell.trashItem).toHaveBeenCalledTimes(1)
-    expect(shell.trashItem).toHaveBeenCalledWith('/system/default/edd-database.sqlite3')
+      expect(shell.trashItem).toHaveBeenCalledTimes(1)
+      expect(shell.trashItem).toHaveBeenCalledWith('/system/default/edd-database.sqlite3')
 
-    expect(app.relaunch).toHaveBeenCalledTimes(1)
-    expect(app.relaunch).toHaveBeenCalledWith()
+      expect(app.relaunch).toHaveBeenCalledTimes(1)
+      expect(app.relaunch).toHaveBeenCalledWith()
 
-    expect(app.exit).toHaveBeenCalledTimes(1)
-    expect(app.exit).toHaveBeenCalledWith(0)
+      expect(app.exit).toHaveBeenCalledTimes(1)
+      expect(app.exit).toHaveBeenCalledWith(0)
+    })
+  })
+
+  describe('when the reset fails', () => {
+    test('logs an error', async () => {
+      const consoleMock = jest.spyOn(console, 'log').mockImplementation(() => {})
+
+      const database = {
+        destroy: jest.fn().mockImplementation(() => {
+          throw new Error('Error')
+        })
+      }
+
+      await resetApplication({ database })
+
+      expect(database.destroy).toHaveBeenCalledTimes(1)
+      expect(database.destroy).toHaveBeenCalledWith()
+
+      expect(app.getPath).toHaveBeenCalledTimes(1)
+      expect(app.getPath).toHaveBeenCalledWith('userData')
+
+      expect(shell.trashItem).toHaveBeenCalledTimes(0)
+      expect(app.relaunch).toHaveBeenCalledTimes(0)
+      expect(app.exit).toHaveBeenCalledTimes(0)
+
+      expect(consoleMock).toHaveBeenCalledTimes(2)
+      expect(consoleMock).toHaveBeenCalledWith('Deleting database file: /system/default/edd-database.sqlite3')
+      expect(consoleMock).toHaveBeenCalledWith('Error deleting database file', new Error('Error'))
+    })
   })
 })

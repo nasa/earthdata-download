f33d6e3377f2f716c74f7279a48bb18eff17919b
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2026-05-05 15:38:36 -0400

    EDD-51: Update deps and improve test coverage (#77)
    * EDD-51: Update deps and improve test coverage
    
    * EDD-51: Bump version

--- src/app/components/Layout/__tests__/Layout.test.js
+++ src/app/components/Layout/__tests__/Layout.test.js
@@ -44,6 +44,12 @@ jest.mock('../../../components/ToastList/ToastList', () => jest.fn(() => (
   <mock-ToastList data-testid="ToastList" />
 )))
 
+jest.mock('../../../pages/FileDownloads/FileDownloads', () => jest.fn(({ children }) => (
+  <mock-FileDownloads data-testid="FileDownloads">
+    {children}
+  </mock-FileDownloads>
+)))
+
 const setup = (overrideApiContextValue = {}, toasts = []) => {
   const addToast = jest.fn((toast) => toasts.push(toast))
   const closeWindow = jest.fn()
@@ -54,6 +60,13 @@ const setup = (overrideApiContextValue = {}, toasts = []) => {
   const setPreferenceFieldValue = jest.fn()
   const showWaitingForEulaDialog = jest.fn()
   const showWaitingForLoginDialog = jest.fn()
+  const beginDownload = jest.fn()
+
+  const callbacks = {}
+  const mockEvent = (eventName, originalFn) => jest.fn((on, callback) => {
+    if (originalFn) originalFn(on, callback)
+    callbacks[eventName] = on ? callback : null
+  })
 
   const apiContextValue = {
     isWin: false,
@@ -64,14 +77,9 @@ const setup = (overrideApiContextValue = {}, toasts = []) => {
     minimizeWindow,
     maximizeWindow,
     setPreferenceFieldValue,
-    showWaitingForEulaDialog,
-    showWaitingForLoginDialog,
+    showWaitingForEulaDialog: mockEvent('showWaitingForEulaDialog', showWaitingForEulaDialog),
+    showWaitingForLoginDialog: mockEvent('showWaitingForLoginDialog', showWaitingForLoginDialog),
     ...overrideApiContextValue
-  }
-
-  const callbacks = {
-    initializeDownload: null,
-    autoUpdateError: null
   }
 
   render(
@@ -80,17 +88,14 @@ const setup = (overrideApiContextValue = {}, toasts = []) => {
         isWin: false,
         isMac: true,
         isLinux: false,
-        autoUpdateAvailable: jest.fn(),
+        autoUpdateAvailable: mockEvent('autoUpdateAvailable'),
         autoUpdateInstallLater: jest.fn(),
-        autoUpdateProgress: jest.fn(),
-        autoUpdateError: jest.fn((on, callback) => {
-          if (on) callbacks.autoUpdateError = callback
-          else callbacks.autoUpdateError = null
-        }),
-        beginDownload: jest.fn(),
-        initializeDownload: jest.fn((on, callback) => { callbacks.initializeDownload = callback }),
-        setDownloadLocation: jest.fn(),
-        windowsLinuxTitleBar: jest.fn(),
+        autoUpdateProgress: mockEvent('autoUpdateProgress'),
+        autoUpdateError: mockEvent('autoUpdateError'),
+        beginDownload,
+        initializeDownload: mockEvent('initializeDownload'),
+        setDownloadLocation: mockEvent('setDownloadLocation'),
+        windowsLinuxTitleBar: mockEvent('windowsLinuxTitleBar'),
         ...apiContextValue
       }
     }
@@ -110,14 +115,16 @@ const setup = (overrideApiContextValue = {}, toasts = []) => {
 
   return {
     addToast,
-    dismissToast,
+    apiContextValue,
+    beginDownload,
     callbacks,
-    getPreferenceFieldValue,
     closeWindow,
-    minimizeWindow,
+    dismissToast,
+    getPreferenceFieldValue,
     maximizeWindow,
-    toasts,
-    setPreferenceFieldValue
+    minimizeWindow,
+    setPreferenceFieldValue,
+    toasts
   }
 }
 
@@ -676,6 +683,213 @@ describe('Layout component', () => {
           })
         ]
       }), {})
+    })
+  })
+
+  describe('when shouldUseDefaultLocation is true', () => {
+    describe('when current page is not Downloads', () => {
+      test('calls beginDownload and addToast', async () => {
+        const user = userEvent.setup()
+        const {
+          callbacks, getPreferenceFieldValue, addToast, beginDownload
+        } = setup()
+
+        getPreferenceFieldValue.mockResolvedValueOnce(true)
+
+        // Navigate away from Downloads page
+        await user.click(screen.getByTestId('layout-button-downloadHistory'))
+
+        await waitFor(() => {
+          callbacks.initializeDownload({ mock: 'event' }, {
+            downloadIds: ['mock-id-1', 'mock-id-2'],
+            downloadLocation: '/mock/location',
+            links: [],
+            shouldUseDefaultLocation: true
+          })
+        })
+
+        await waitFor(() => {
+          expect(getPreferenceFieldValue).toHaveBeenCalledTimes(1)
+          expect(beginDownload).toHaveBeenCalledTimes(1)
+          expect(beginDownload).toHaveBeenCalledWith({
+            downloadIds: ['mock-id-1', 'mock-id-2'],
+            downloadLocation: '/mock/location',
+            makeDefaultDownloadLocation: true
+          })
+
+          expect(addToast).toHaveBeenCalledWith(expect.objectContaining({
+            id: 'new-download-mock-id-1',
+            title: 'Download started',
+            message: 'mock-id-1'
+          }))
+
+          expect(addToast).toHaveBeenCalledWith(expect.objectContaining({
+            id: 'new-download-mock-id-2',
+            title: 'Download started',
+            message: 'mock-id-2'
+          }))
+        })
+      })
+    })
+
+    describe('when current page is Downloads', () => {
+      test('calls beginDownload but does not add download toasts', async () => {
+        const {
+          callbacks, getPreferenceFieldValue, addToast, beginDownload
+        } = setup()
+
+        getPreferenceFieldValue.mockResolvedValueOnce(true)
+
+        await waitFor(() => {
+          callbacks.initializeDownload({ mock: 'event' }, {
+            downloadIds: ['mock-download'],
+            downloadLocation: '/mock/location',
+            links: [],
+            shouldUseDefaultLocation: true
+          })
+        })
+
+        await waitFor(() => {
+          expect(beginDownload).toHaveBeenCalledTimes(1)
+          expect(addToast).not.toHaveBeenCalledWith(expect.objectContaining({
+            title: 'Download started'
+          }))
+        })
+      })
+    })
+  })
+
+  describe('when shouldUseDefaultLocation is false', () => {
+    test('does not call beginDownload and sets page to Downloads', async () => {
+      const user = userEvent.setup()
+      const { callbacks, getPreferenceFieldValue, beginDownload } = setup()
+
+      getPreferenceFieldValue.mockResolvedValueOnce(true)
+
+      // Navigate away from Downloads page
+      await user.click(screen.getByTestId('layout-button-downloadHistory'))
+
+      // We should be on DownloadHistory now
+      expect(DownloadHistory).toHaveBeenCalledTimes(1)
+
+      await waitFor(() => {
+        callbacks.initializeDownload({ mock: 'event' }, {
+          downloadIds: ['mock-id'],
+          downloadLocation: '/mock/location',
+          links: [],
+          shouldUseDefaultLocation: false
+        })
+      })
+
+      await waitFor(() => {
+        expect(beginDownload).not.toHaveBeenCalled()
+        // Should navigate back to Downloads page plus the original initial load
+        expect(Downloads).toHaveBeenCalledTimes(2)
+      })
+    })
+  })
+
+  describe('auto-update handling', () => {
+    test('displays a toast when an update is available', async () => {
+      const { callbacks, addToast } = setup()
+
+      await waitFor(() => {
+        callbacks.autoUpdateAvailable()
+      })
+
+      await waitFor(() => {
+        expect(addToast).toHaveBeenCalledWith(expect.objectContaining({
+          id: 'updateAvailable',
+          title: 'An app update is available',
+          variant: 'spinner'
+        }))
+      })
+    })
+
+    test('displays progress toast during auto-update', async () => {
+      const { callbacks, addToast } = setup()
+
+      await waitFor(() => {
+        callbacks.autoUpdateProgress({}, { percent: 50 })
+      })
+
+      await waitFor(() => {
+        expect(addToast).toHaveBeenCalledWith(expect.objectContaining({
+          id: 'updateAvailable',
+          message: 'Downloading update: 50%',
+          variant: 'spinner'
+        }))
+      })
+    })
+
+    test('displays success toast when auto-update is ready', async () => {
+      jest.useFakeTimers()
+      const { callbacks, addToast, dismissToast } = setup()
+
+      await waitFor(() => {
+        callbacks.autoUpdateProgress({}, { percent: 100 })
+      })
+
+      await waitFor(() => {
+        expect(addToast).toHaveBeenCalledWith(expect.objectContaining({
+          id: 'updateAvailable',
+          message: 'Update is ready to install',
+          variant: 'success'
+        }))
+      })
+
+      // This is needed because of the `setInterval` timer in Layout.jsx
+      jest.advanceTimersByTime(3000)
+      expect(dismissToast).toHaveBeenCalledWith('updateAvailable')
+      jest.useRealTimers()
+    })
+  })
+
+  describe('download location events', () => {
+    test('updates selected download location', async () => {
+      const { callbacks } = setup()
+
+      await waitFor(() => {
+        callbacks.setDownloadLocation({}, { downloadLocation: '/new/path' })
+      })
+    })
+  })
+
+  describe('waiting dialog events', () => {
+    test('shows waiting for EULA dialog', async () => {
+      const { callbacks } = setup()
+
+      await waitFor(() => {
+        callbacks.showWaitingForEulaDialog({}, {
+          showDialog: true,
+          downloadId: 'mock-download-id'
+        })
+      })
+
+      expect(screen.getByText('Accept the license agreement to continue your download.')).toBeInTheDocument()
+    })
+
+    test('shows waiting for login dialog', async () => {
+      const { callbacks } = setup()
+
+      await waitFor(() => {
+        callbacks.showWaitingForLoginDialog({}, {
+          showDialog: true,
+          downloadId: 'mock-download-id'
+        })
+      })
+
+      expect(screen.getByText('You must log in with Earthdata Login to download this data.')).toBeInTheDocument()
+    })
+  })
+
+  describe('window state events', () => {
+    test('updates maximized state', async () => {
+      const { callbacks } = setup()
+
+      await waitFor(() => {
+        callbacks.windowsLinuxTitleBar({}, true)
+      })
     })
   })
 })

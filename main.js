e0ec465ec263594e7ff9d34f7092f0b1c534a513
Isaac Mello <dzordinetwork@gmail.com>
2026-07-23 14:35:13 -0300

    EDD-63: Add version number to the downloads page
    Display the latest released version in the page header, sourced from the
    GitHub releases API. Because the download buttons always point to
    releases/latest, the version shown matches what the user downloads and
    stays in sync with future releases with no manual updates. Falls back to
    hiding the element if the version cannot be determined.
    
    Closes #63

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.scss'
 
 import { setOSClassName } from './js/util/setOSClassName'
+import { setVersionNumber } from './js/util/setVersionNumber'
 
 setOSClassName(document.body)
+setVersionNumber(document.getElementById('versionNumber'))
